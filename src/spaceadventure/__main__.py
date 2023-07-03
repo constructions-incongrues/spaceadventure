5ccd9c7e9c6ca7bafb7de9dbc50a88e27893f31d
Tristan Rivoallan <tristan@rivoallan.net>
2023-07-03 23:52:14 +0200

    feer

--- src/spaceadventure/__main__.py
+++ src/spaceadventure/__main__.py
@@ -28,7 +28,7 @@ def title(screen):
         Print(screen,
               SpeechBubble("Appuyez sur ENTREE pour commencer"),
               y=screen.height - 3,
-              start_frame=5)
+              start_frame=15)
     ]
 
     return Scene(effects, -1)
